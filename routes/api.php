c16e491e82f0aa8683fa4e128269467a9f978abf
yussef12 <youssefamad8@gmail.com>
2024-06-28 19:03:24 +0200

    add authentification

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
+Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
+Route::post('/me', [AuthController::class, 'me'])->middleware('auth:api')->name('me');
